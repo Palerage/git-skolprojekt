948439d494f7e27401acd2c0a13483023e0e0afe
Royne <royne@outlook.com>
2022-09-11 11:48:03 +0200

    Skapat en Entity Class för Course

---
+++ Entity/Course.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Course
+    {
+        public Guid Id {get; set;}
+
+        public string Title { get; set; }
+
+        public float Price { get; set; }
+
+        public string Instructor { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string Image { get; set; }
+    }
+}
