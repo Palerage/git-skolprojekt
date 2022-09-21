7805b5f919f5c4d3a9548f3394c5a6f2a2eb733d
Royne <royne@outlook.com>
2022-09-21 22:29:43 +0200

    Modul 7 Part 4: Creating Categories Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import DetailPage from './pages/DetailPage';
@@ -6,6 +6,7 @@ import Homepage from './pages/Homepage';
 import LoginPage from './pages/Login';
 import 'antd/dist/antd.css';
 import Categories from './components/Categories';
+import CategoryPage from './pages/CategoryPage';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route exact path="/" component={Categories} />
       <Switch>
         <Route exact path="/" component={Homepage}/>
+        <Route exact path="/category/:id" component={CategoryPage}/>
         <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/detail" component={DetailPage}/>
       </Switch>
--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from "react";
 import agent from "../actions/agent";
+import { Link } from "react-router-dom";
 import { Category } from "../models/category";
 
 const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
 
-    const [categories, setCategories] = useState<Category[]>([]);
+  useEffect(() => {
+    agent.Categories.list().then((response) => {
+      setCategories(response);
+    });
+  }, []);
 
-    useEffect(() =>{
-
-        agent.Categories.list().then((response) => {
-            setCategories(response);
-        });
-
-    }, []);
-
-    return (
-        <div className="categories">
-            {categories && categories.map((category: Category, index: number) =>{
-                return (<div key={index} className="categories__name">{category.name}</div>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="categories">
+      {categories &&
+        categories.map((category: Category, index: number) => {
+          return (
+            <Link key={index} to={`/category/${category.id}`}>
+              <div className="categories__name"> {category.name}</div>
+            </Link>
+          );
+        })}
+    </div>
+  );
+};
 
 export default Categories;
