b3924017e192992549bff74b133b7615114189ad
Royne <royne@outlook.com>
2022-09-21 22:46:29 +0200

    Modul 7 Part 5: Creating Description Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from './pages/Login';
 import 'antd/dist/antd.css';
 import Categories from './components/Categories';
 import CategoryPage from './pages/CategoryPage';
+import DescriptionPage from "./pages/DescriptionPage";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route exact path="/category/:id" component={CategoryPage}/>
+        <Route exact path="/course/:id" component={DescriptionPage}/>
         <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/detail" component={DetailPage}/>
       </Switch>
