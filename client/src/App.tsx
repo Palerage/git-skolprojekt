568125270bc0d7e6ca2adfd03005c4c1f6d1f9d0
Royne <royne@outlook.com>
2022-09-23 16:27:50 +0200

    Modul 8 Part 6: Creating Basket Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,27 @@
-import React from "react";
 import { Route, Switch } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import 'antd/dist/antd.css';
+import './sass/main.scss';
 import DetailPage from './pages/DetailPage';
 import Homepage from './pages/Homepage';
-import LoginPage from './pages/Login';
-import 'antd/dist/antd.css';
+import LoginPage from './pages/LoginPage';
+import Navigation from './components/Navigation';
 import Categories from './components/Categories';
 import CategoryPage from './pages/CategoryPage';
-import DescriptionPage from "./pages/DescriptionPage";
-
+import DescriptionPage from './pages/DescriptionPage';
+import BasketPage from './pages/BasketPage';
 
 function App() {
-
   return (
     <>
-      <Navigation/>
+      <Navigation />
       <Route exact path="/" component={Categories} />
       <Switch>
-        <Route exact path="/" component={Homepage}/>
-        <Route exact path="/category/:id" component={CategoryPage}/>
-        <Route exact path="/course/:id" component={DescriptionPage}/>
-        <Route exact path="/login" component={LoginPage}/>
-        <Route exact path="/detail" component={DetailPage}/>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/course/:id" component={DescriptionPage} />
+        <Route exact path="/category/:id" component={CategoryPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/detail" component={DetailPage} />
+        <Route exact path="/basket" component={BasketPage} />
       </Switch>
     </>
   );
