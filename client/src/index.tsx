57be2f5ace556e46a992ad3031ed5770839f22e9
Royne <royne@outlook.com>
2022-09-23 20:43:38 +0200

    Modul 9 Part 1: Installing Redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 //import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './context/StoreContext';
+import { configureStore } from './redux/store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <StoreProvider>
+        <Provider store = {store}>
         <App />
+        </Provider>
       </StoreProvider>
     </Router>
   </React.StrictMode>,
