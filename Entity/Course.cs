f02c0cb77207063ec1d9742be645a7722e665d8c
Royne <royne@outlook.com>
2022-09-26 18:39:50 +0200

    Module 11 Part 9: Purchase Courses Endpoint

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -37,5 +37,6 @@ namespace Entity
         public Category Category { get; set; }
 
         public DateTime LastUpdated { get; set; } = DateTime.Now;
+        public ICollection<UserCourse> UserCourses { get; set; }
     }
 }
---
+++ Infrastructure/Migrations/20220926161923_UserCoursesAdded.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class UserCoursesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "309de59a-0f7b-4bda-b8ee-21979161a902");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3698ef14-bff1-4c81-bc9d-9bc75813580a");
+
+            migrationBuilder.CreateTable(
+                name: "UserCourses",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    CourseId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCourses", x => new { x.UserId, x.CourseId });
+                    table.ForeignKey(
+                        name: "FK_UserCourses_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "563cfd8f-3ba0-4f3d-84d1-2b693f72aeee", "60921136-66b1-4975-8fe4-44fadf6b556c", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "fc4890d7-b5aa-409f-a82b-d6a606ef6bb4", "e88592c2-e2cd-4f21-b544-1ab11631f0fc", "Instructor", "INSTRUCTOR" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCourses_CourseId",
+                table: "UserCourses",
+                column: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCourses");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "563cfd8f-3ba0-4f3d-84d1-2b693f72aeee");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fc4890d7-b5aa-409f-a82b-d6a606ef6bb4");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "309de59a-0f7b-4bda-b8ee-21979161a902", "85e44d49-cea8-412d-bec3-c2ad586302c5", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "3698ef14-bff1-4c81-bc9d-9bc75813580a", "02838dba-82a9-4dbc-b110-0479f6c7cf49", "Instructor", "INSTRUCTOR" });
+        }
+    }
+}
