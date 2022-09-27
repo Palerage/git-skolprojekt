b371da53a2a679982f072de66d17b15b54dc1d60
Royne <royne@outlook.com>
2022-09-27 15:57:36 +0200

    Module 12 Part 1: Displaying User courses

--- client/src/components/ShowCourses.tsx
+++ client/src/components/ShowCourses.tsx
@@ -16,6 +16,8 @@ const ShowCourses = ({ course }: Props) => {
   const { basket } = useAppSelector((state) => state.basket);
   const dispatch = useAppDispatch();
 
+  const {userCourses} = useAppSelector((state) => state.user)
+
   const checkWidth = (): void => {
     if (window.innerWidth > 1024) {
       setSpanVal(6);
@@ -62,7 +64,10 @@ const ShowCourses = ({ course }: Props) => {
           </div>
           <div className="course__bottom">
             <div className="course__bottom__price">{course.price}</div>
-            {basket?.items.find((item) => item.courseId === course.id) !==
+            {userCourses?.find((item: Course) => item.id === course.id) != undefined ?
+            
+          (<div className="course__bottom__cart">Go to course</div>) :
+            basket?.items.find((item) => item.courseId === course.id) !==
             undefined ? (
               <Link to="/basket">
                 <div className="course__bottom__cart">Go to Cart</div>
@@ -76,7 +81,8 @@ const ShowCourses = ({ course }: Props) => {
               >
                 Add to cart
               </div>
-            )}
+            )
+          }
           </div>
         </Card>
       </Col>
